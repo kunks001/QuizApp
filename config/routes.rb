d4b75f967d076a0dbff28de60ac481ce98bc33d6
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-10-08 17:18:06 +0100

    Added static_pages controller and route to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 QuizApp::Application.routes.draw do
-  get "quizzes/new"
+  root  'static_pages#home'
+  match '/new_quiz',  to: 'quizzes#new',            via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
