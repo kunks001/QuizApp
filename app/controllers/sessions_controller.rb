596ad3ab157564625730e0e752dbc08c881186fc
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-10-10 21:35:36 +0100

    Added remember token and sign-in functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-  end
+	  user = User.find_by(email: params[:session][:email].downcase)
+	  if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
+    else
+	    render 'new'
+	  end
+	end
 
   def destroy
   end
