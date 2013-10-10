eefa701ce1e279d76654da40e42f1f6dc37aeb69
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-10-10 17:38:40 +0100

    Added user password function, tests & more validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131010154046) do
+ActiveRecord::Schema.define(version: 20131010163353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(version: 20131010154046) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
