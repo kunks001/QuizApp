6442d5236e855d48c7e92bfdcc20507d9101a1f2
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-10-10 16:54:29 +0100

    Added user model, first tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131009153528) do
+ActiveRecord::Schema.define(version: 20131010154046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 20131009153528) do
 
   create_table "quizzes", force: true do |t|
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
