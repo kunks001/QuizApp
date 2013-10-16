0662e36e66ed6d9bf54ca27a2721066330a90464
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-10-16 22:56:56 +0100

    Added user_id column to quizzes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131010203035) do
+ActiveRecord::Schema.define(version: 20131016215032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20131010203035) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe Quiz do
+	let(:user) { FactoryGirl.create(:user) }
 
   before do
-    @quiz = Quiz.new(title: "makers_quiz")
+    @quiz = Quiz.new(title: "makers_quiz", user_id: user.id)
   end
 
   subject { @quiz }
 
   it { should respond_to(:title) }
   it { should have_many(:questions) }
+  it { should respond_to(:user_id) }
 end
