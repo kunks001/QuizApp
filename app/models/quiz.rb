80ff95ff65edc7be225221de0c1c2aa6bb1741c0
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-10-16 23:02:38 +0100

    added user/model association &  validation tests

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,7 @@
 class Quiz < ActiveRecord::Base
+	belongs_to :user
+	validates :user_id, presence: true
+	
 	has_many :questions, :dependent => :destroy
 	accepts_nested_attributes_for :questions, :allow_destroy => true, :reject_if => lambda { |a| a[:query].blank? }
 end
--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -2,14 +2,18 @@ require 'spec_helper'
 
 describe Quiz do
 	let(:user) { FactoryGirl.create(:user) }
-
-  before do
-    @quiz = Quiz.new(title: "makers_quiz", user_id: user.id)
-  end
+	before { @quiz = user.quizzes.build(title: "Makers Quiz") }
 
   subject { @quiz }
 
   it { should respond_to(:title) }
   it { should have_many(:questions) }
   it { should respond_to(:user_id) }
+  it { should respond_to(:user) }
+  its(:user) { should eq user }
+
+  describe "when user_id is not present" do
+    before { @quiz.user_id = nil }
+    it { should_not be_valid }
+  end
 end
